2e9dba3b15c6737d5bc770b32ac747d1af8ca854
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2021-11-13 15:57:04 -0500

    Added comments to script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// declaring variables for getting input from Form IDs
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
 const includeNumbersElement = document.getElementById('includeNumbers')
@@ -5,9 +7,11 @@ const includeSpecialElement = document.getElementById('includeSpecial')
 const form = document.getElementById('passwordGenForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
 
+// declares variables as arrays using ASCII Character Codes. Selects the ASCII character codes from their code numbers.
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+// This concat new arrays to select the different ASCII character code ranges of the special characters 
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(58, 64)
 ).concat(
@@ -18,7 +22,9 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 
+// Adds event listener to the Generate Password Button. On submit it will call the generatePassword function variables. .Checked just tells us if it is True or False that the checkbox has been selected. 
 form.addEventListener('submit', e => {
+  // prevent default stops the button press from refreshing the page
   e.preventDefault()
   const characterAmount = characterAmountNumber.value
   const includeUppercase = includeUppercaseElement.checked
@@ -28,20 +34,26 @@ form.addEventListener('submit', e => {
   passwordDisplay.innerText = password
 })
 
+// This takes the ASCII character codes and passes the data to the password field.
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSpecial) {
+  // sets charCodes and appends the additional arrays by concatenating the Uppercase, Symbol and Numbers. 
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeSpecial) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
+  // stores password characters in empty array.
   const passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
+    // gets random value from the entire charCodes array
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
+  // turns array into empty string
   return passwordCharacters.join('')
 }
 
+// generates array from the low number to the high number of the ASCII character codes.
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -50,7 +62,9 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
+// function to sync the character amount event argument and gets value then sets the character amount and range to a value. 
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
+  characterAmountRange.value = value
 }
